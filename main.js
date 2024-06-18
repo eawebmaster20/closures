2567a29fb52cc097be26cd666fd804441d61d31a
eawebmaster20 <eaweb20@gmail.com>
2024-06-18 04:17:51 -0700

    feat: created a new person using the 'Person' object function

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ function Person (name, age){
 Person.prototype.greet = function () {
     console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`)
 }
+
+
+let person = new Person("Benjamin", 5)
+person.greet();
