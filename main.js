d2235425eacef84aabd7e0c694aa9cc41a833147
eawebmaster20 <eaweb20@gmail.com>
2024-06-18 04:23:20 -0700

    feat: applied '.call()' on the greet() function

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ Person.prototype.greet = function () {
     console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`)
 }
 
-
 let person = new Person("Benjamin", 5)
 person.greet();
+person.greet.call({name:'mensah',age:21});
